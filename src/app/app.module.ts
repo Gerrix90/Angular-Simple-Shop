a70e91ce7da41a7eb913a28ccf448b868a37155b
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-27 14:45:23 +0300

    added category-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CategoryCardComponent } from './category/category-card/category-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CategoryCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/category/category-card/category-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'db-category-card',
+  templateUrl: './category-card.component.html',
+  styleUrls: ['./category-card.component.css']
+})
+export class CategoryCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
