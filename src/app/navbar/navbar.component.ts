4757e652ad660fef29657012bfb66d9eea82d2d2
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-27 14:39:33 +0300

    added navbar template and describe navbarComponent class

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface NavItem {
+  // Navigation link
+  href: string;
+  // Navigation Label
+  label: string;
+  // Status of Navigation Item
+  active: boolean;
+}
+
 @Component({
   selector: 'db-navbar',
   templateUrl: './navbar.component.html',
@@ -11,5 +20,17 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  // App name
+  appName: string = 'Dream Bean';
+
+  // Navgation items
+  navItems: NavItem[] = [
+    {href: '#', label: 'Home', active: true},
+    {href: '#', label: 'Products', active: false},
+    {href: '#', label: 'Checkout', active: false},
+    {href: '#', label: 'Sign out', active: false}
+  ];
+
 
 }
