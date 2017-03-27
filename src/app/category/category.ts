90bd7b1ca3c266675eb0798768f810e5a613e1a4
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-27 14:43:01 +0300

    added model for category

---
+++ src/app/category/category.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by SergST on 27.03.2017.
+ */
+
+
+export class Category {
+  // Unique Id
+  id: string;
+  // The title
+  title: string;
+  // Description
+  desc: string;
+  // Path to image
+  image: string;
+}
