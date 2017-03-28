0aeb71af5f51e02a58883fc484c4ef5036b77e2a
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-28 15:48:30 +0300

    added routing and welcome component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {CategoryModule} from "./category/category.module";
+import {appRouting} from "./app.routing";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { WelcomeComponent } from './welcome/welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    CategoryModule
+    CategoryModule,
+    appRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by SergST on 28.03.2017.
+ */
+
+import {Routes, RouterModule} from "@angular/router";
+import {WelcomeComponent} from "./welcome/welcome/welcome.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent },
+];
+
+export const appRouting = RouterModule.forRoot(routes);
