b1281d68cf2d65c9f753b3d5ed04701af05b03e5
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-30 23:14:42 +0300

    create product service from product.ts file

--- src/app/product/product-grid/product-grid.component.ts
+++ src/app/product/product-grid/product-grid.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {getProducts, Product} from "../product";
+import {getProducts, Product} from "../product.service";
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
