36a42750e80642bac58741991f05ae9a43075155
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-30 22:20:01 +0300

    create category service from category.ts file

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -8,6 +8,7 @@ import {CommonModule} from "@angular/common";
 import {CategoryCardComponent} from "./category-card/category-card.component";
 import {CategorySliderComponent} from "./category-slider/category-slider.component";
 import {CategoryListComponent} from "./category-list/category-list.component";
+import {CategoryService} from "./category";
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import {CategoryListComponent} from "./category-list/category-list.component";
     CategoryCardComponent,
     CategorySliderComponent,
     CategoryListComponent
-  ]
+  ],
+  providers: [CategoryService]
 })
 export class CategoryModule{}
--- src/app/category/category.ts
+++ src/app/category/category.ts
@@ -2,6 +2,8 @@
  * Created by SergST on 27.03.2017.
  */
 
+import {Injectable} from "@angular/core";
+
 
 export class Category {
   // Unique Id
@@ -16,29 +18,69 @@ export class Category {
   imageL: string;
 }
 
-const categories: Category[]  = [
-  { id: '1', title: 'Bread & Bakery', imageL: 'http://placehold.it/1110x480', imageS: 'http://placehold.it/270x171', desc: 'The best cupcakes, cookies, cakes, pies, cheesecakes, fresh bread, biscotti, muffins, bagels, fresh coffee and more.' },
-  { id: '2', title: 'Takeaway', imageL: 'http://placehold.it/1110x480', imageS: 'http://placehold.it/270x171', desc: "It's consistently excellent, dishes are superb and healthily cooked with high quality ingredients." },
-  { id: '3', title: 'Dairy', imageL: 'http://placehold.it/1110x480', imageS: 'http://placehold.it/270x171', desc: 'A dairy product is food produced from the milk of mammals, primarily cows, water buffaloes, goats, sheep, yaks.' },
-  { id: '4', title: 'Meat', imageL: 'http://placehold.it/1110x480', imageS: 'http://placehold.it/270x171', desc: 'Only superior quality beef, lamb, pork.' },
-  { id: '5', title: 'Seafood', imageL: 'http://placehold.it/1110x480', imageS: 'http://placehold.it/270x171', desc: 'Great place to buy fresh seafood.' },
-  { id: '6', title: 'Fruit & Veg', imageL: 'http://placehold.it/1110x480', imageS: 'http://placehold.it/270x171', desc: 'A variety of fresh fruits and vegetables.' }
-]
+@Injectable()
+  export class CategoryService {
 
-export function getCategories(): Category[] {
-  return categories;
-}
-
-
-export function getCategory(id: string): Category {
-  for (let i = 0; i < categories.length; i++) {
-    if (categories[i].id === id) {
-      return categories[i];
+  categories: Category[] = [
+    {
+      id: '1',
+      title: 'Bread & Bakery',
+      imageL: 'http://placehold.it/1110x480',
+      imageS: 'http://placehold.it/270x171',
+      desc: 'The best cupcakes, cookies, cakes, pies, cheesecakes, fresh bread, biscotti, muffins, bagels, fresh coffee and more.'
+    },
+    {
+      id: '2',
+      title: 'Takeaway',
+      imageL: 'http://placehold.it/1110x480',
+      imageS: 'http://placehold.it/270x171',
+      desc: "It's consistently excellent, dishes are superb and healthily cooked with high quality ingredients."
+    },
+    {
+      id: '3',
+      title: 'Dairy',
+      imageL: 'http://placehold.it/1110x480',
+      imageS: 'http://placehold.it/270x171',
+      desc: 'A dairy product is food produced from the milk of mammals, primarily cows, water buffaloes, goats, sheep, yaks.'
+    },
+    {
+      id: '4',
+      title: 'Meat',
+      imageL: 'http://placehold.it/1110x480',
+      imageS: 'http://placehold.it/270x171',
+      desc: 'Only superior quality beef, lamb, pork.'
+    },
+    {
+      id: '5',
+      title: 'Seafood',
+      imageL: 'http://placehold.it/1110x480',
+      imageS: 'http://placehold.it/270x171',
+      desc: 'Great place to buy fresh seafood.'
+    },
+    {
+      id: '6',
+      title: 'Fruit & Veg',
+      imageL: 'http://placehold.it/1110x480',
+      imageS: 'http://placehold.it/270x171',
+      desc: 'A variety of fresh fruits and vegetables.'
     }
-  }
-  throw new CategoryNotFoundException(`Category ${id} not found`);
-}
+  ]
 
+
+  getCategories(): Category[] {
+    return this.categories;
+  }
+
+
+  getCategory(id: string): Category {
+    for (let i = 0; i < this.categories.length; i++) {
+      if (this.categories[i].id === id) {
+        return this.categories[i];
+      }
+    }
+    throw new CategoryNotFoundException(`Category ${id} not found`);
+  }
+}
 
 export class CategoryNotFoundException extends Error {
   constructor(message?: string) {
