e10192dce68c8e22ffe3232647261e391f4e1e7c
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-31 00:05:28 +0300

    added interface CardItem in shopping cartService

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -2,6 +2,14 @@
  * Created by SergST on 30.03.2017.
  */
 
+import {Product} from "../product/product.service";
+
+
+export interface CartItem {
+  product: Product;
+  count: number;
+  amount: number;
+}
 
 export class Cart {
   count: number = 0;
