97b7f47af43f7037b7f4f4dc2d0e76373054acd3
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-04-01 02:29:35 +0300

    added CategoryTitle Pipe

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,11 +5,13 @@
 import {Routes, RouterModule} from "@angular/router";
 import {WelcomeComponent} from "./welcome/welcome.component";
 import {ProductListComponent} from "./product/product-list/product-list.component";
+import {ProductViewComponent} from "./product/product-view/product-view.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'products', component: ProductListComponent },
+  {path: 'products/:id', component: ProductViewComponent},
 ];
 
 export const appRouting = RouterModule.forRoot(routes);
--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -9,6 +9,7 @@ import {CategoryCardComponent} from "./category-card/category-card.component";
 import {CategorySliderComponent} from "./category-slider/category-slider.component";
 import {CategoryListComponent} from "./category-list/category-list.component";
 import {CategoryService} from "./category.service";
+import {CategoryTitlePipe} from './category-title.pipe';
 
 
 @NgModule({
@@ -18,12 +19,14 @@ import {CategoryService} from "./category.service";
   declarations: [
     CategoryCardComponent,
     CategorySliderComponent,
-    CategoryListComponent
+    CategoryListComponent,
+    CategoryTitlePipe
   ],
   exports: [
     CategoryCardComponent,
     CategorySliderComponent,
-    CategoryListComponent
+    CategoryListComponent,
+    CategoryTitlePipe
   ],
   providers: [CategoryService]
 })
