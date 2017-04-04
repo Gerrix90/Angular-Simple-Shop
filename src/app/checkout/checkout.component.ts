307920b34985c0c3a3532968a0d8231183779fed
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-04-04 11:59:18 +0300

    added form and cart properties in checkout component

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Cart, CartService} from "../cart/cart.service";
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'db-checkout',
@@ -7,10 +9,22 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() {
+  cart: Cart;
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private cartService: CartService) {
+    this.cart = this.cartService.cart
   }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required],
+      address: []
+    })
   }
 
 }
