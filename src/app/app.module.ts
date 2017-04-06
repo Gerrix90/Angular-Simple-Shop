59ebe93003fa2ffede015bdad1d8c2d714d53990
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-04-06 16:57:59 +0300

    added and init firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,20 @@ import {CheckoutComponent} from './checkout/checkout.component';
 import {InMemoryDataService} from "./shared/in-memory-data.service";
 import {InMemoryWebApiModule} from "angular2-in-memory-web-api";
 
+import {AngularFireModule} from 'angularfire2';
+
+
+// Initialize Firebase
+export const fbConfig = {
+  apiKey: "AIzaSyBRcjZHOBpufc33mJ1ZX3Ur4ICs_i-ICnQ",
+  authDomain: "simple-store-12e8e.firebaseapp.com",
+  databaseURL: "https://simple-store-12e8e.firebaseio.com",
+  projectId: "simple-store-12e8e",
+  storageBucket: "simple-store-12e8e.appspot.com",
+  messagingSenderId: "250747019841"
+};
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +46,8 @@ import {InMemoryWebApiModule} from "angular2-in-memory-web-api";
     appRouting,
     ProductModule,
     CartModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService, {delay: 1000})
+    InMemoryWebApiModule.forRoot(InMemoryDataService, {delay: 1000}),
+    AngularFireModule.initializeApp(fbConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
